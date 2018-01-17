0fdc042c1325f547f3c81b162ff5d5e7e154ccc2
Vitaly Dragun <vdrag00n90@gmail.com>
2018-01-17 15:34:07 +0200

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,62 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// define model for User
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email!'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+// override existing method on a schema
+// this method defines what exactly gets send back when the mongoose
+// model is converted into a json value
+UserSchema.methods.toJSON = function () {
+    let user = this;
+    let userObject = user.toObject();   // create object from user schema
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+// create custom instance method on our schema
+// don't use arrow function because we need access to 'this' keyword
+UserSchema.methods.generateAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({access, token});
+    // return promise which contains auth. token
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+// define model for User
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
